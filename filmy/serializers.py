06f493cdcbd38186629e175abe35ee562abbac9c
Klimek <klimkowskib@gmail.com>
2020-04-26 18:19:39 +0200

    Make 2 serializer (list and on index), API ready to connect with Frontend

--- filmy/serializers.py
+++ filmy/serializers.py
@@ -4,4 +4,10 @@ from .models import Film
 class FilmSerializers(serializers.ModelSerializer):
     class Meta:
         model = Film
+        fields = ['id', 'plakat']
+
+
+class FilmFullSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Film
         fields = ['id','tytul','opis','rok', 'plakat']
--- filmy/views.py
+++ filmy/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from .serializers import FilmSerializers
+from .serializers import FilmSerializers, FilmFullSerializers
 from rest_framework import viewsets
 from .models import Film
+from rest_framework.response import Response
 # Create your views here.
 
 
 class FilmViewset(viewsets.ModelViewSet):
     serializer_class = FilmSerializers
     queryset = Film.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):   #def z ctrl plus klik na ModelViewSet na RetriveModelMixin >  vievsets.py > mixins.py
+        film = self.get_object()
+        serializer = FilmFullSerializers(film)   #tu wklejamy FillFull zeby po kliknieciu GET na element (tj po przejsciu w indeks elementu wyswieltilty sie wszystkie elementy)
+        return Response(serializer.data)
