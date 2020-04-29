b170062f62f74e64df47536952417fd18f5cd4cd
Klimek <klimkowskib@gmail.com>
2020-04-29 17:32:49 +0200

    Add media to upload img to angular

--- filmapi/urls.py
+++ filmapi/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('filmy.urls')), #bo stworzylismy plik urls w filmy i tam bedzie nas przekierowywac
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT) #dodanie Media Url i Media Root z settings.py by (aby dodac zdjecia z media/plakaty)
